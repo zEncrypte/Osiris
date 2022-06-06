d4fe6e9f6397dbc848c3dac92c367393ce807ed1
zEncrypte <72712659+zEncrypte@users.noreply.github.com>
2022-06-06 04:48:42 -0500

    update user agent

--- modules/bumper.py
+++ modules/bumper.py
@@ -5,7 +5,7 @@ from colorama import Fore
 
 def bumper():
     headers = {
-        'User-Agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7',
+        'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0',
         'Authorization' : input(f"\n [>] Token: ")
     }
 
