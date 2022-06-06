f157f97ee2c0da9f64219598dc565ddb198b7b55
zEncrypte <72712659+zEncrypte@users.noreply.github.com>
2022-06-06 05:04:29 -0500

    Update credits.py

--- modules/credits.py
+++ modules/credits.py
@@ -1,7 +1,7 @@
 def show_credits():
     print(f"\n [+] https://linktr.ee/FeelLucky")
     print(f"\n [+] billythegoat | Pyfade")
-    print(f"\n [+] https://github.com/zEncrypte | me:3)
+    print(f"\n [+] https://github.com/zEncrypte | me:3")
 
 def show_help():
     pass
