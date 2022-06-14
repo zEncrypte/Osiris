5528bbed31853036267485927dfa466912fd613b
Dan <72712659+zEncrypte@users.noreply.github.com>
2022-06-13 19:39:10 -0500

    Update commands.py

--- modules/commands.py
+++ modules/commands.py
@@ -1,14 +1,14 @@
-from colorama import Fore, Style
+from pystyle import Colors
 def ayuda():
-    print(Fore.RED + Style.BRIGHT + f"\n [+] TokenRaper: Destruye una cuenta por completo.")
-    print(f"\n [+] WebhookSpammer: Spamea un webhook especificado.")
-    print(f"\n [+] TokenChecker: Saca la información de una cuenta + verifica si el token es válido.")
-    print(f"\n [+] WebhookChecker: Revisa la información de un webhook + verifica si el webhook es válido.")
-    print(f"\n [+] HistoryClear: Elimina tu historial de mensajes en un canal especificado")
-    print(f"\n [+] AutoBump: Envia el comando !d bump cada 2 horas en un canal especificado.")
-    print(f"\n [+] TokenGrabber: Crea un token grabber con la webhook especificado.")
-    print(f"\n [+] ServerLookup: Revisa la información de un servidor en el que esta el usuario.")
-    print(f"\n [+] MassReport: Realiza un reporte masivo")
+    print(f"\n{Colors.light_red} [+] TokenRaper: Destruye una cuenta por completo.")
+    print(f"\n{Colors.light_red} [+] WebhookSpammer: Spamea un webhook especificado.")
+    print(f"\n{Colors.light_red} [+] TokenChecker: Saca la información de una cuenta + verifica si el token es válido.")
+    print(f"\n{Colors.light_red} [+] WebhookChecker: Revisa la información de un webhook + verifica si el webhook es válido.")
+    print(f"\n{Colors.light_red} [+] HistoryClear: Elimina tu historial de mensajes en un canal especificado")
+    print(f"\n{Colors.light_red} [+] AutoBump: Envia el comando !d bump cada 2 horas en un canal especificado.")
+    print(f"\n{Colors.light_red} [+] TokenGrabber: Crea un token grabber con la webhook especificado.")
+    print(f"\n{Colors.light_red} [+] ServerLookup: Revisa la información de un servidor en el que esta el usuario.")
+    print(f"\n{Colors.light_red} [+] MassReport: Realiza un reporte masivo")
 
 
 def show_help():
