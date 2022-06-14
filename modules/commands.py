0cf0338c103c3bccd9b60a93259274ef36e6f018
Dan <72712659+zEncrypte@users.noreply.github.com>
2022-06-13 19:43:58 -0500

    re color

--- modules/commands.py
+++ modules/commands.py
@@ -1,14 +1,14 @@
 from pystyle import Colors
 def ayuda():
-    print(f"\n{Colors.light_red} [+] TokenRaper: Destruye una cuenta por completo.")
-    print(f"\n{Colors.light_red} [+] WebhookSpammer: Spamea un webhook especificado.")
-    print(f"\n{Colors.light_red} [+] TokenChecker: Saca la información de una cuenta + verifica si el token es válido.")
-    print(f"\n{Colors.light_red} [+] WebhookChecker: Revisa la información de un webhook + verifica si el webhook es válido.")
-    print(f"\n{Colors.light_red} [+] HistoryClear: Elimina tu historial de mensajes en un canal especificado")
-    print(f"\n{Colors.light_red} [+] AutoBump: Envia el comando !d bump cada 2 horas en un canal especificado.")
-    print(f"\n{Colors.light_red} [+] TokenGrabber: Crea un token grabber con la webhook especificado.")
-    print(f"\n{Colors.light_red} [+] ServerLookup: Revisa la información de un servidor en el que esta el usuario.")
-    print(f"\n{Colors.light_red} [+] MassReport: Realiza un reporte masivo")
+    print(f" {Colors.purple}[{Colors.light_blue}+{Colors.purple}]{Colors.light_red} TokenRaper: Destruye una cuenta por completo.")
+    print(f" {Colors.purple}[{Colors.light_blue}+{Colors.purple}]{Colors.light_red} WebhookSpammer: Spamea un webhook especificado.")
+    print(f" {Colors.purple}[{Colors.light_blue}+{Colors.purple}]{Colors.light_red} TokenChecker: Saca la información de una cuenta + verifica si el token es válido.")
+    print(f" {Colors.purple}[{Colors.light_blue}+{Colors.purple}]{Colors.light_red} WebhookChecker: Revisa la información de un webhook + verifica si el webhook es válido.")
+    print(f" {Colors.purple}[{Colors.light_blue}+{Colors.purple}]{Colors.light_red} HistoryClear: Elimina tu historial de mensajes en un canal especificado")
+    print(f" {Colors.purple}[{Colors.light_blue}+{Colors.purple}]{Colors.light_red} AutoBump: Envia el comando !d bump cada 2 horas en un canal especificado.")
+    print(f" {Colors.purple}[{Colors.light_blue}+{Colors.purple}]{Colors.light_red} TokenGrabber: Crea un token grabber con la webhook especificado.")
+    print(f" {Colors.purple}[{Colors.light_blue}+{Colors.purple}]{Colors.light_red} ServerLookup: Revisa la información de un servidor en el que esta el usuario.")
+    print(f" {Colors.purple}[{Colors.light_blue}+{Colors.purple}]{Colors.light_red} MassReport: Realiza un reporte masivo")
 
 
 def show_help():
