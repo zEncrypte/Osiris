1c57ee25e7b9805e73b42edf23a4c4c6c230f38c
Dan <72712659+zEncrypte@users.noreply.github.com>
2022-06-16 15:07:02 -0500

    Update serverLookup.py

--- modules/serverLookup.py
+++ modules/serverLookup.py
@@ -1,16 +1,16 @@
-from colorama import Fore
-import requests
-import time
-import sys
+import sys, time, requests
+from pystyle import Colors, System, Cursor
 
 
 def fetch_data():
+    System.Title("Server Lookup :: Osiris")
+    Cursor.HideCursor()
     headers = {
         'User-Agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7',
-        'Authorization' : input(Fore.BLUE + f"\n [>] Token: " + Fore.RESET)
+        'Authorization' : input(f"\n {Colors.purple}[{Colors.light_blue}>{Colors.purple}] Token: {Colors.white}")
     }
 
-    guildId = input(Fore.BLUE + f" [>] ID del servidor: "+ Fore.RESET)
+    guildId = input(f" {Colors.purple}[{Colors.light_blue}>{Colors.purple}] ID del servidor: {Colors.white}")
 
     response = requests.get(
         f"https://discord.com/api/guilds/{guildId}",
@@ -25,13 +25,13 @@ def fetch_data():
     ).json()
 
     print(f"""
-    Nombre del servidor: {response['name']} ~ {response['id']}
-    URL del icono: https://cdn.discordapp.com/icons/{guildId}/{response['icon']}.webp?size=256
-    Cantidad aproximada de miembros: {response['approximate_member_count']}
-    Owner: {owner['user']['username']}#{owner['user']['discriminator']} ~ {response['owner_id']}
-    Region: {response['region']}
-    Invitacion personalizada: {response['vanity_url_code']}
-""")
+    {Colors.purple}Nombre del servidor: {Colors.orange}{response['name']} {Colors.light_blue}~{Colors.orange} {response['id']}
+    {Colors.purple}Icono del servidor: {Colors.orange}https://cdn.discordapp.com/icons/{guildId}/{response['icon']}.webp?size=256
+    {Colors.purple}Cantidad aproximada de miembros: {Colors.orange}{response['approximate_member_count']}
+    {Colors.purple}Propietario: {Colors.orange}{owner['user']['username']}{Colors.light_blue}#{Colors.orange}{owner['user']['discriminator']} {Colors.light_blue}~{Colors.orange} {response['owner_id']}
+    {Colors.purple}Region: {Colors.orange}{response['region']}
+    {Colors.purple}Invitacion personalizada: {Colors.orange}{response['vanity_url_code']}
+{Colors.white}""")
 
 if __name__ == '__main__':
     fetch_data()
